24c945b24b410900dc1a1682ced2f0dcbf901e09
Reyhan <88860417+SFZIBO@users.noreply.github.com>
2025-03-21 10:43:36 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    name = ""
+    message = ""
+
+    if request.method == "POST":  # Pastikan metode POST diterima
+        name = request.form.get("name")
+        message = request.form.get("message")
+
+    return render_template("index.html", name=name, message=message)
+
+if __name__ == '__main__' :
+    app.run(debug=True)
